9e8e0af045605679a5cba5ba219971ae9d1e9ce1
danicor2688 <danicor2688@gmail.com>
2025-05-20 13:28:11 -0500

    Se aplica terraform fmt

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "backend-terraform-networking"
-    key            = "resources-networking/dev/terraform.tfstate"
-    region         = "us-east-1"
-    use_lockfile   = true
+    bucket       = "backend-terraform-networking"
+    key          = "resources-networking/dev/terraform.tfstate"
+    region       = "us-east-1"
+    use_lockfile = true
   }
 }
