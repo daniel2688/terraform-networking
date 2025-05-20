c5e6f49007e29f087f81262300fccd3d77ae10f3
danicor2688 <danicor2688@gmail.com>
2025-05-20 12:09:39 -0500

    Formatear archivos terraform

--- vpc_subnets.tf
+++ vpc_subnets.tf
@@ -12,7 +12,7 @@ variable "vpc_cidr" {
 }
 
 resource "aws_vpc" "main" {
-  cidr_block           = var.vpc_cidr
+  cidr_block = var.vpc_cidr
   tags = {
     Name = "main-vpc"
   }
@@ -20,10 +20,10 @@ resource "aws_vpc" "main" {
 
 # 🔹 Subredes públicas (2)
 resource "aws_subnet" "public" {
-  count             = 3
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = cidrsubnet(var.vpc_cidr, 3, count.index)
-  availability_zone = data.aws_availability_zones.available.names[count.index]
+  count                   = 3
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = cidrsubnet(var.vpc_cidr, 3, count.index)
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
   map_public_ip_on_launch = true
 
   tags = {
